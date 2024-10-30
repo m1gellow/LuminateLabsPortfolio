e9cf4374802535cf8d928e9ff5c7ec66b9587beb
Artem <xfan08@mail.ru>
2024-10-30 23:52:42 +0800

    added google file

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Shared/Navbar";
 import MyWorks from "./components/Shared/MyWorks";
+import Head from "next/head";
 
 const roboto = Roboto({
   variable: "--font-robot",
@@ -25,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+      <meta name="google-site-verification" content="EObO9EN98MC7TntRPHorF5mfeh9aRE6-OoRVUB7VQ8I" />
+      </Head>
       <body className={`${roboto.variable} antialiased`} >
           <div className="flex justify-center">
             <Navbar />
